edee3931bbb8acaeeb9f99691578ea47db53d69b
welingtonnes <140860957+welingtonnes@users.noreply.github.com>
2024-05-22 21:38:36 -0300

    Add files via upload

---
+++ atividade/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded",function() {
+    const images = document.querySelectorAll('img');
+    images.forEach(img => console.log(img));
+    });
+    
+    const infoParagraph = document.getElementById('info');
+    infoParagraph.textContent = 'Aqui estão cinco destinos incríveis para a sua próxima viagem. Cada um oferece experiências únicas e inesquecíveis.';
